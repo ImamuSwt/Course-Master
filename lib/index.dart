945fa6bc7df5fddecc84d6ba2ba611b3c5d9cdfa
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2023-10-28 15:44:08 +0000

    Updating to latest FlutterFlow output.

---
+++ lib/index.dart
@@ -0,0 +1,33 @@
+// Export pages
+export '/pages/cs151/cs151_widget.dart' show Cs151Widget;
+export '/pages/courseslist/courseslist_widget.dart' show CourseslistWidget;
+export '/pages/cs1180/cs1180_widget.dart' show Cs1180Widget;
+export '/pages/cs125/cs125_widget.dart' show Cs125Widget;
+export '/pages/quiz/quiz_widget.dart' show QuizWidget;
+export '/pages/courseslist_copy/courseslist_copy_widget.dart'
+    show CourseslistCopyWidget;
+export '/pages/profile/profile_widget.dart' show ProfileWidget;
+export '/pages/edit_profile/edit_profile_widget.dart' show EditProfileWidget;
+export '/pages/book_c_s125/book_c_s125_widget.dart' show BookCS125Widget;
+export '/pages/c_s125_video/c_s125_video_widget.dart' show CS125VideoWidget;
+export '/pages/chapter2_vid_c_s125/chapter2_vid_c_s125_widget.dart'
+    show Chapter2VidCS125Widget;
+export '/pages/chapter3_vid_c_s125/chapter3_vid_c_s125_widget.dart'
+    show Chapter3VidCS125Widget;
+export '/pages/chapter4_vid_c_s125/chapter4_vid_c_s125_widget.dart'
+    show Chapter4VidCS125Widget;
+export '/pages/chaoter5_vid_c_s125/chaoter5_vid_c_s125_widget.dart'
+    show Chaoter5VidCS125Widget;
+export '/pages/quiz_copy/quiz_copy_widget.dart' show QuizCopyWidget;
+export '/pages/quiz_copy_copy/quiz_copy_copy_widget.dart'
+    show QuizCopyCopyWidget;
+export '/pages/excercise/excercise_widget.dart' show ExcerciseWidget;
+export '/pages/c_s125_exerciseschapter1/c_s125_exerciseschapter1_widget.dart'
+    show CS125Exerciseschapter1Widget;
+export '/pages/c_s125_exercises_chapter2/c_s125_exercises_chapter2_widget.dart'
+    show CS125ExercisesChapter2Widget;
+export '/pages/c_s125_execises_chapter3/c_s125_execises_chapter3_widget.dart'
+    show CS125ExecisesChapter3Widget;
+export '/pages/c_s125_execises_chapter4/c_s125_execises_chapter4_widget.dart'
+    show CS125ExecisesChapter4Widget;
+export '/pages/authenticat/authenticat_widget.dart' show AuthenticatWidget;
